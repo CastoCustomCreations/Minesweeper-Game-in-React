dc25ccab9585f6aaf567c29398ceec9642cc5b5e
Jen Casto <CastoCustomCreations@gmail.com>
2023-06-13 11:47:43 -0400

    Delete src directory

--- src/containers/index.js
+++
@@ -1,4 +0,0 @@
-export { default as BoardContainer } from './board/Board';
-export { default as CellContainer } from './board/Cell';
-export { default as SettingsContainer } from './settings/Settings';
-export { default as StatusContainer } from './status/Status';
--- src/store/modules/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import { default as controlReducer } from './control';
-
-export const rootReducer = combineReducers({
-	control: controlReducer
-});
